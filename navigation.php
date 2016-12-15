429cf89ad98f8d2a1af9b4e94160cff75025ae11
karimulla <karimulla@gmail.com>
2016-12-14 21:58:35 -0500

    fix navigation urls

--- navigation.php
+++ navigation.php
@@ -6,12 +6,12 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">My Pharmacy</a>
+          <a class="navbar-brand" href="/">My Pharmacy</a>
         </div>
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
             <li class="active">
-              <a href="#">Home</a>
+              <a href="/">Home</a>
             </li>
             <li>
               <a href="admin.php">Admin Reports</a>
@@ -20,7 +20,7 @@
               <a href="reports.php">Reports</a>
             </li>
             <li>
-              <a href="#contact">Contact</a>
+              <a href="/">Contact</a>
             </li>
           </ul>
         </div>
