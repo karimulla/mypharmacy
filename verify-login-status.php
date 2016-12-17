ebd99071168e499d03f8da4fa639e6b4f61aa72b
Karimulla Shaik <karimulla@gmail.com>
2016-12-17 07:07:26 -0500

    adding authorization check

--- verify-login-status.php
+++ verify-login-status.php
@@ -1,5 +1,8 @@
+<?php
 session_start();
 if((isset($_SESSION['valid_user']) && !$_SESSION['valid_user'])  || (!isset($_SESSION['valid_user']))) {
   header('Location: index.php');
   die();
 }
+
+?>
