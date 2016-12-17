8a4faa41dfc40441ff50fe45db6d920db6081eee
Karimulla Shaik <karimulla@gmail.com>
2016-12-16 22:04:17 -0500

    updating database info for login pages

--- include/membersite_config.php
+++ include/membersite_config.php
@@ -4,7 +4,7 @@ require_once("./include/fg_membersite.php");
 $fgmembersite = new FGMembersite();
 
 //Provide your site name here
-$fgmembersite->SetWebsiteName('user11.com');
+$fgmembersite->SetWebsiteName('mypharmacytest.azurewebsites.net');
 
 //Provide the email address where you want to get notifications
 $fgmembersite->SetAdminEmail('user11@user11.com');
@@ -13,11 +13,11 @@ $fgmembersite->SetAdminEmail('user11@user11.com');
 //hostname, user name, password, database name and table name
 //note that the script will create the table (for example, fgusers in this case)
 //by itself on submitting register.php for the first time
-$fgmembersite->InitDB(/*hostname*/'localhost',
-                      /*username*/'prasanth',
-                      /*password*/'p',
-                      /*database name*/'testdb',
-                      /*table name*/'fgusers3');
+$fgmembersite->InitDB(/*hostname*/'mypharmacytest.database.windows.net',
+                      /*username*/'sqladmin',
+                      /*password*/'admin123!',
+                      /*database name*/'mypharmacytest2',
+                      /*table name*/'dbo.SalesMen');
 
 //For better security. Get a random string from this link: http://tinyurl.com/randstr
 // and put it here
